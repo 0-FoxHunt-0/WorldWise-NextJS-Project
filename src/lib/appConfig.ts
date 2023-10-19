c509721b2b72e75363085011737db200c92ab1a0
Nate <nethanelstahlberg@gmail.com>
2023-10-19 13:11:59 +0300

    production baseURL change

--- src/lib/appConfig.ts
+++ src/lib/appConfig.ts
@@ -1,5 +1,7 @@
 class AppConfig {
-  public baseUrl: string = "http://localhost:3000";
+  // public baseUrl: string = "http://localhost:3000";
+  public baseUrl: string =
+    "https://worldwide-qef7ayyco-nates-projects-4a12ab74.vercel.app";
 }
 
 const appConfig = new AppConfig();
