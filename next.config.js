6cedbdc7a93d7e336b10cd3b9702cdad42c8cc83
Nate <nethanelstahlberg@gmail.com>
2023-10-24 00:31:20 +0300

    CORS fix attempt 1

--- next.config.js
+++ next.config.js
@@ -14,7 +14,7 @@ const nextConfig = {
         source: "/api/:path*",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "*" }, // replace this your actual origin
+          { key: "Access-Control-Allow-Origin", value: "https://worldwise-lemon.vercel.app" }, // replace this your actual origin
           {
             key: "Access-Control-Allow-Methods",
             value: "GET,DELETE,PATCH,POST,PUT",
