2605fca2ecf2431d636ca7dac983c57af6b7a099
Nate <nethanelstahlberg@gmail.com>
2023-10-24 00:37:57 +0300

    CORS fix attempt 2

--- src/lib/appConfig.ts
+++ src/lib/appConfig.ts
@@ -1,7 +1,6 @@
 class AppConfig {
   // public baseUrl: string = "http://localhost:3000";
-  public baseUrl: string =
-    "https://worldwide-git-master-nates-projects-4a12ab74.vercel.app";
+  public baseUrl: string = "https://worldwise-lemon.vercel.app/";
 }
 
 const appConfig = new AppConfig();
