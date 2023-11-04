19c24294960e3a1be03f2af918bc727012696690
Nate <nethanelstahlberg@gmail.com>
2023-11-04 12:14:47 +0200

    Next 14 migration

--- next.config.js
+++ next.config.js
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    serverActions: true,
-  },
   env: {
     API_KEY: process.env.API_KEY,
     BIN_ID: process.env.BIN_ID,
